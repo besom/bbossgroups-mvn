9eba580599686954b6b08ac55b2a52f842b97106
ijoyful <gaotang@192.168.1.103>
2014-12-16 10:42:13 +0800

    add schedule

---
+++ bboss_schedule/src/main/java/org/frameworkset/task/CalendarBuilder.java
@@ -0,0 +1,35 @@
+/*
+ *  Copyright 2008 biaoping.yin
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package org.frameworkset.task;
+
+import org.frameworkset.spi.ApplicationContextAware;
+import org.quartz.Calendar;
+
+/**
+ * <p>Title: CalendarBuilder.java</p> 
+ * <p>Description: </p>
+ * <p>bboss workgroup</p>
+ * <p>Copyright (c) 2007</p>
+ * @Date 2013-1-23 ÉÏÎç10:09:08
+ * @author biaoping.yin
+ * @version 1.0
+ */
+public interface CalendarBuilder extends ApplicationContextAware{
+	public String getCalendarName();
+	public Calendar buildCalendar();
+
+}
